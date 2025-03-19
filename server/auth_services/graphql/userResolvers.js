af4cd807bc192f9f04259f261407b035a98f194f
Sebastian <sebastian_bdavid@hotmail.com>
2025-03-19 19:37:45 -0400

    fix: fixed stupid comment bug

--- server/auth_services/graphql/userResolvers.js
+++ server/auth_services/graphql/userResolvers.js
@@ -10,9 +10,8 @@ const resolvers = {
       if (!token) {
         return null; // No user is logged in
       }
-
       try {
-        // Verify and decode the JWT. Note: Make sure to handle errors appropriately in a real app
+        // Verify and decode the JWT
         const decoded = jwt.verify(token, "your_secret_key");
         return { username: decoded.username };
       } catch (error) {
@@ -21,29 +20,30 @@ const resolvers = {
       }
     },
   },
-
   Mutation: {
     login: async (_, { username, password }, { res }) => {
-      // In a real app, validate username and password against a database
+      // Validate username and password against database
       const user = await User.findOne({ username });
       if (!user) {
         throw new Error("User not found");
       }
-
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
         throw new Error("Invalid password");
       }
-      //
+      
+      // Token generation
       const token = jwt.sign({ username }, "your_secret_key", {
         expiresIn: "1d",
       });
+      
       res.cookie("token", token, {
         httpOnly: true,
-        //sameSite: 'None',
-        // secure: true, // Set to true if your site is served over HTTPS
+        // sameSite: 'None',  // Enable for cross-site requests
+        // secure: true,      // Enable for HTTPS
         maxAge: 24 * 60 * 60 * 1000, // 1 day
       });
+      
       return true;
     },
     register: async (_, { username, password }) => {
@@ -51,7 +51,6 @@ const resolvers = {
       if (existingUser) {
         throw new Error("Username is already taken");
       }
-
       const hashedPassword = await bcrypt.hash(password, 10);
       const newUser = new User({ username, password: hashedPassword });
       await newUser.save();
@@ -59,3 +58,5 @@ const resolvers = {
     },
   },
 };
+
+module.exports = resolvers;
