ac54940f10eb7d56ed2b4007e6b5ce696d31601b
Sebastian <sebastian_bdavid@hotmail.com>
2025-03-19 18:41:00 -0400

    feat: setup project and database connection

---
+++ server/vital_sign_services/vital-microservice.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+// const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+// const { ApolloServer } = require("@apollo/server");
+// const { startStandaloneServer } = require("@apollo/server/standalone");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Connect to mongodb
+mongoose
+  .connect(process.env.MONGODB_VITALS_URI)
+  .then(() => {
+    console.log("Connected to MongoDB vitals service");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
